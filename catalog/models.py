3d247b0229fcba187102a5131dd6db2306b6dc99
pafnootiy <ksander-86@mail.ru>
2022-07-04 12:47:56 +0300

    add models

--- catalog/models.py
+++ catalog/models.py
@@ -19,6 +19,30 @@ class User(models.Model):
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200,
+                               null=True,
+                               blank=True
+                               )
+    time = models.DateTimeField(auto_now=True)
+    approx_size = models.IntegerField(null=False)
+    comment = models.TextField(max_length=200,
+                               null=True,
+                               blank=True
+                               )
+    paid_till = models.DateTimeField(null=False)
+    tariff = models.IntegerField(null=False)
+    size = models.CharField(max_length=200,
+                            null=True,
+                            blank=True
+                            )
+    deleted = models.BooleanField()
+    qr_code = models.CharField(max_length=200,
+                               null=True,
+                               blank=False)
+    status = models.CharField(max_length=200,
+                               null=True,
+                               blank=True)
 
     def __str__(self):
         return f'Заказ {self.pk}'
